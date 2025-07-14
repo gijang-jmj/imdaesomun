fc50aa226e23b749f81e81a81bc5a057180eaf1a
jmj <wnalsals1127@gmail.com>
2025-07-14 17:08:26 +0900

    task: exclude ios userAgent in document viewer

--- lib/src/ui/pages/webview/document_viewer.dart
+++ lib/src/ui/pages/webview/document_viewer.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:imdaesomun/src/data/models/file.dart';
@@ -12,55 +14,42 @@ class DocumentViewer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final WebViewController controller;
     final Uri uri = Uri.parse(file.fileLink);
-    final String userAgent =
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36';
+
+    final WebViewController controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onSslAuthError: (request) {
+            if (file.fileLink.contains('bmc.busan.kr')) {
+              request.proceed();
+            } else {
+              request.cancel();
+            }
+          },
+        ),
+      );
+
+    if (Platform.isAndroid) {
+      final String userAgent =
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36';
+      controller.setUserAgent(userAgent);
+    }
 
     if (file.fileId != null && file.fileId!.isNotEmpty) {
       final Uint8List body = Uint8List.fromList(
         utf8.encode('attachNo=${file.fileId}'),
       );
-      controller =
-          WebViewController()
-            ..setJavaScriptMode(JavaScriptMode.unrestricted)
-            ..setUserAgent(userAgent)
-            ..setNavigationDelegate(
-              NavigationDelegate(
-                onSslAuthError: (request) {
-                  if (file.fileLink.contains('bmc.busan.kr')) {
-                    request.proceed();
-                  } else {
-                    request.cancel();
-                  }
-                },
-              ),
-            )
-            ..loadRequest(
-              uri,
-              method: LoadRequestMethod.post,
-              headers: <String, String>{
-                'Content-Type': 'application/x-www-form-urlencoded',
-              },
-              body: body,
-            );
+      controller.loadRequest(
+        uri,
+        method: LoadRequestMethod.post,
+        headers: <String, String>{
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: body,
+      );
     } else {
-      controller =
-          WebViewController()
-            ..setJavaScriptMode(JavaScriptMode.unrestricted)
-            ..setUserAgent(userAgent)
-            ..setNavigationDelegate(
-              NavigationDelegate(
-                onSslAuthError: (request) {
-                  if (file.fileLink.contains('bmc.busan.kr')) {
-                    request.proceed();
-                  } else {
-                    request.cancel();
-                  }
-                },
-              ),
-            )
-            ..loadRequest(uri);
+      controller.loadRequest(uri);
     }
 
     return Scaffold(
